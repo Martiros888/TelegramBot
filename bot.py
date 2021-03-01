136e5696e53da0211755e8259539071b63b9abe2
Martiros888 <65274148+Martiros888@users.noreply.github.com>
2021-03-01 05:53:13 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ import jwt
 import requests
 link = 'http://192.168.1.22:3000/tokens/'
 
-questions_enlish = ['name','surname','age']
+questions_english = ['name','surname','age']
 questions_russian = ['имя','фамилия','возраст']
 
 users = {}
@@ -22,7 +22,6 @@ bot = telebot.TeleBot(config.TOKEN)
 def start(message):   
     global users
     users = {**users, message.from_user.id:{}}
-    r = types.InlineQueryResultArticle('1', 'Result1', types.InputTextMessageContent('hi'))
     markup = types.InlineKeyboardMarkup(row_width=2)
     button1 = types.InlineKeyboardButton('Russian',callback_data='russian')
     button2 = types.InlineKeyboardButton('English',callback_data='english')
@@ -39,63 +38,85 @@ def questions(message):
 @bot.callback_query_handler(func=lambda call:True)
 def callback_line(call):
     if call.message:
+        print(call.data)
         if call.data == 'english':
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            button1 = types.InlineKeyboardButton('Saint Petersburg',callback_data='petersburg_english')
+            button2 = types.InlineKeyboardButton('Moscow',callback_data='moscow_english')
+            markup.add(button1,button2)
             bot.delete_message(chat_id=call.message.chat.id,message_id=call.message.message_id)
-            bot.send_message(call.message.chat.id,'you choose English')
-            # bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,reply_markup=None,text='please enter your name, surname, passport')
+            bot.send_message(call.message.chat.id,'you choose English',reply_markup=markup)
+            return 
         elif call.data == 'russian':
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            button1 = types.InlineKeyboardButton('Санкт-Петербург',callback_data='petersburg_russian')
+            button2 = types.InlineKeyboardButton('Москва',callback_data='moscow_russian')
+            markup.add(button1,button2)
             bot.delete_message(chat_id=call.message.chat.id,message_id=call.message.message_id)
-            bot.send_message(call.message.chat.id,'you choose Russian')
-            # bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,reply_markup=None,text='please enter your name, surname, passport')
-
+            bot.send_message(call.message.chat.id,'вы выбрали русский язык',reply_markup=markup)
+            return 
+        data = call.data.split('_')
+        users[call.message.chat.id]['info'] = {
+            'language':data[1],
+            'city':data[0],
+        }
+        if data[1] == 'russian':
+            bot.send_message(call.message.chat.id,'пожалуйста, нажмите на /questions, чтобы ответить на вопросы')
+            return 
+        bot.send_message(call.message.chat.id,'please click on /questions for asking questions')
+                   
+    
 
 
 @bot.message_handler(content_types=['text'])
 def send_message(message):
-    if 'questions' in users[message.from_user.id]:
-        user = users[message.from_user.id]
-        user[user['questions'][len(user['questions']) - 1]] = message.text
-        print(user['questions'])
-        if len(user['questions']) == len(questions_enlish):
-            del user['questions'] 
-            encoded_jwt = jwt.encode(user,'secretkey',algorithm='HS256')
-            qr = qrcode.QRCode(version=1,box_size=10,border=5)
-            qr.add_data(link+encoded_jwt)
-            qr.make(fit=True)
-            img = qr.make_image(fill='black',back_color='white')
-            random_name = 'img'+str(random())+'.png'
-            img.save('images/'+random_name)
-            img = open('./images/'+random_name,'rb')
-            bot.send_photo(message.chat.id,img,reply_markup=None)
-            bot.send_message(message.chat.id,link+encoded_jwt)
-            return
-        bot.send_message(message.chat.id,'please enter your '+ questions_enlish[len(user['questions'])])
-        user['questions'] = [*user['questions'], questions_enlish[len(user['questions'])]]
     print(users)
+    if users[message.chat.id]['info']['language'] == 'english':    
+        if 'questions' in users[message.from_user.id]:
+            user = users[message.from_user.id]
+            user[user['questions'][len(user['questions']) - 1]] = message.text
+            if len(user['questions']) == len(questions_english):
+                del user['questions'] 
+                encoded_jwt = jwt.encode(user,'secretkey',algorithm='HS256')
+                qr = qrcode.QRCode(version=1,box_size=10,border=5)
+                qr.add_data(link + encoded_jwt)
+                qr.make(fit=True)
+                img = qr.make_image(fill='black',back_color='white')
+                random_name = 'img'+str(random())+'.png'
+                img.save('images/'+random_name)
+                img = open('./images/'+random_name,'rb')
+                bot.send_photo(message.chat.id,img,reply_markup=None)
+                bot.send_message(message.chat.id,link + encoded_jwt)
+                return
+            bot.send_message(message.chat.id,'please enter your '+ questions_english[len(user['questions'])])
+            user['questions'] = [*user['questions'], questions_english[len(user['questions'])]]
+        return 
+    if users[message.chat.id]['info']['language'] == 'russian':
+        if 'questions' in users[message.from_user.id]:
+            user = users[message.from_user.id]
+            user[user['questions'][len(user['questions']) - 1]] = message.text
+            if len(user['questions']) == len(questions_russian):
+                del user['questions'] 
+                encoded_jwt = jwt.encode(user,'secretkey',algorithm='HS256')
+                qr = qrcode.QRCode(version=1,box_size=10,border=5)
+                qr.add_data(link + encoded_jwt)
+                qr.make(fit=True)
+                img = qr.make_image(fill='black',back_color='white')
+                random_name = 'img'+str(random())+'.png'
+                img.save('images/'+random_name)
+                img = open('./images/'+random_name,'rb')
+                bot.send_photo(message.chat.id,img,reply_markup=None)
+                bot.send_message(message.chat.id,link + encoded_jwt)
+                return
+            bot.send_message(message.chat.id,'Пожалуйста, введите Ваше '+ questions_russian[len(user['questions'])])
+            user['questions'] = [*user['questions'], questions_russian[len(user['questions'])]]
+            return 
+    bot.send_message(message.chat.id,'/questions')        
 
-
-        # print(questions_enlish[len(user['questions'])])
-        # user['questions'] = [*user['questions'], questions_enlish[len(user['questions']-1)]]
-        # bot.send_message(message.chat.id,'please enter your'+questions_enlish[len(user['questions'])])
-    # if len(users[message.from_user.id].questions) != 0:
-    #     print('this is answer question')
-    # data = message.text.split('-')
-    # res = requests.post('http://192.168.1.22:8888/gettoken',data={'name':data[0],'surname':data[1],'passport_number':data[2]})
-    # if data[0].strip() == 'username':
-    # if data[0].strip() == 'name':
-    # bot.send_message(message.chat.id,'wait please')
-    # encoded_jwt = jwt.encode({'name':data[0],'surname':data[1],'passport_number':data[2]},'secretkey',algorithm='HS256')
-    # qr = qrcode.QRCode(version=1,box_size=10,border=5)
-    # qr.add_data(link+encoded_jwt)
-    # qr.make(fit=True)
-    # img = qr.make_image(fill='black',back_color='white')
-    # random_name = 'img'+str(random())+'.png'
-    # img.save('images/'+random_name)
-    # img = open('./images/'+random_name,'rb')
-    # bot.send_photo(message.chat.id,img,reply_markup=None)
-    # bot.send_message(message.chat.id,link+encoded_jwt)
 
 
 
 bot.polling(none_stop=True)
+
+
 
